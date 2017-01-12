c265257816e1e452ca218aa268c72c34c875742e
Andrew Walker <awalkerdev@gmail.com>
2017-01-12 15:16:24 +0000

    update most copy

--- assets/js/init.js
+++ assets/js/init.js
@@ -179,31 +179,6 @@ $(document).ready(function(){
         remove: false
     });
 
-    $("#mapwrapper").gMap({
-        controls: true,
-        scrollwheel: false,
-        markers: [{
-            latitude: $latitude,
-            longitude: $longitude,
-            icon: {
-                image: "assets/img/marker.png",
-                iconsize: [44,44],
-                iconanchor: [12,46],
-                infowindowanchor: [12, 0]
-            }
-        }],
-        icon: {
-            image: "assets/img/marker.png",
-            iconsize: [26, 46],
-            iconanchor: [12, 46],
-            infowindowanchor: [12, 0]
-        },
-        latitude: $latitude,
-        longitude: $longitude,
-        zoom: 13,
-        styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-    });
-
     //SIDE NAV MOBILE
     $('#side-menu-toggle').on('click', function(event){
       event.preventDefault();
